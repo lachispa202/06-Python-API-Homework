3c3e653a4dfb90552f0eb8b8fa1edc9743e234c9
Tobias Judd <tobias_judd@hotmail.com>
2020-09-11 10:42:26 -0500

    Code to generate lat/long and create +500 unique cities

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "5a8b6e05154de2bab21dca3a810c51a3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD7hIXnlM1KkDRaqtWycGzUy9OsykWclh4"
